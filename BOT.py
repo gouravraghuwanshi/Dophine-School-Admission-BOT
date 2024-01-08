26c246e5ae1548939c7bdf47565f88d9d980dd13
gouravraghuwanhi <82863058+gouravraghuwanshi@users.noreply.github.com>
2024-01-08 13:56:47 +0530

    Update and rename botforfolphin.py to BOT.py
    cleared the clutter

--- BOT.py
+++ BOT.py
@@ -4,56 +4,64 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-# Replace the path below with the location of your chromedriver executable
-driver = webdriver.Chrome()
+import configparser
 
+#configration data of student
+config=configparser.ConfigParser()
+config.read('config.ini')
+name1 = config['USER_DATA']['name']
+birth_year = config['USER_DATA']["birth_year"]
+DOB_MONTH = config['USER_DATA']["DOB_MONTH"]
+DOB_DATE = config['USER_DATA']['DOB_DATE']
+gender = config['USER_DATA']['gender']
+father_name = config['USER_DATA']['Father_name']
+mother_name = config['USER_DATA']['Mother_name']
+father_number = config['USER_DATA']['Father_number']
+mother_number = config['USER_DATA']['Mother_number']
+# Replace the path below with the location of your chromedriver executable
+driver = webdriver.Firefox()
 # Load the webpage
 driver.get('http://reg.dolphinbasoda.org/Application.aspx')
 
-
 # Fill in the name field
 name_field = driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_tbxStudentName')
-name_field.send_keys('YOUR NAME')
+name_field.send_keys(name1)
+
 
 # Find the date of birth field and click it
 dob_field = driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_tbxDateOfBirth')
 dob_field.click()
 
-
-
 # Find the date picker widget and select the desired month and year
 dob_calendar = driver.find_element(By.ID, 'ui-datepicker-div')
 dob_calendar.find_element(By.CLASS_NAME, 'ui-datepicker-year').click()
-dob_calendar.find_element(By.XPATH, '//option[text()="YEAR IN NUMBERS LIKE 2020"]').click()#YEAR 
-
-
+xpath_expression = f'//option[text()="{birth_year}"]'
+dob_calendar.find_element(By.XPATH, xpath_expression).click()
 dob_calendar.find_element(By.CLASS_NAME, 'ui-datepicker-month').click()
-dob_calendar.find_element(By.XPATH, '//option[text()="MONTH NAME ONLY THREE LETTERS LIKE (dec)"]').click() #MONTH NAME
-
+dob_calendar.find_element(By.XPATH, f'//option[text()="{DOB_MONTH}"]').click()
 
 # Find and click the desired date
-dob_calendar.find_element(By.XPATH, '//a[text()="DATE IT the date is 01 then only type 1 "]').click() #DATE
+dob_calendar.find_element(By.XPATH, f'//a[text()={DOB_DATE}]').click()
 
-# Select the gender
-
-
+#This code is for gender
 gender_dropdown = Select(driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_cboGender'))
-gender_dropdown.select_by_value('FEMALE')  #SPECIFY THE GENDER
+gender_dropdown.select_by_value(f'{gender}')
 
 
 # Fill in the father's name field
 father_name_field = driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_tbxFatherName')
-father_name_field.send_keys('FATHERS NAME') 
+father_name_field.send_keys(f'{father_name}')
+
 
 
 
 # Fill in the mobile number field
 mobile_field = driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_tbxCellPhone1')
-mobile_field.send_keys('00000000') #MOBILE NUMBER ONLY 10 DIGITS NO CONTRY CODE
+mobile_field.send_keys(f'{father_number}')
+
 # Fill in the mobile number field
 mobile_field = driver.find_element(By.ID, 'ctl00_ContentPlaceHolder1_tbxCellPhone2')
-mobile_field.send_keys('9876543210') #MOBILE NUMBER ONLY 10 DIGITS NO CONTRY CODE
-
+mobile_field.send_keys(f'{mother_number}')
 
 
 
@@ -62,9 +70,3 @@ mobile_field.send_keys('9876543210') #MOBILE NUMBER ONLY 10 DIGITS NO CONTRY COD
 submit_button = driver.find_element(By.ID, 'btnSave')
 submit_button.click()
 
-
-# Print the success message
-time.sleep(15)
-
-# # Close the browser
-driver.quit()
